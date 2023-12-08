3245278fe5df70307d211b743b968f8f3d851513
Quinlan <quinnbush5@gmail.com>
2023-12-07 18:01:00 -0800

    pixel-art-BE fix

--- app/models/pixel_art_model.py
+++ app/models/pixel_art_model.py
@@ -4,6 +4,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class PixelArtModel(db.Model):
     __tablename__ = "pixel_art_models"
 
+    if environment == "production":
+        __table_args__ = {"schema": SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     image_url = db.Column(db.String)
